120c0f4fdc04b90173d1f88a1205b0bac2e459ea
yltx ll <yltxll@example.com>
2021-03-27 19:46:47 +0800

    init gshop

---
+++ src/main.js
@@ -0,0 +1,13 @@
+// The Vue build version to load with the `import` command
+// 入口文件
+import Vue from 'vue'
+import App from './App'
+import router from './router'
+
+/* eslint-disable no-new */
+new Vue({
+  el: '#app',
+  components: { App },
+  template: '<App/>',
+  router
+})
